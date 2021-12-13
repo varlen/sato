6d9a3b0a104052c7f92235de154bf64cb3ed8f82
Varlen <varlenneto@poli.ufrj.br>
2021-12-12 22:08:20 -0300

    Fix function name

--- demo/quickrun.py
+++ demo/quickrun.py
@@ -3,6 +3,6 @@ import pandas as pd
 import os
 from predict import evaluate
 
-df = pd.read_csv(os.path.join(UPLOAD_FOLDER, 'employees.csv'))
+df = pd.read_csv(os.path.join(UPLOAD_FOLDER, 'theaters.csv'))
 res = evaluate(df)
 print(res)
